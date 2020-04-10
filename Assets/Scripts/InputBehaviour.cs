f71173c5d61a3e3447a9e2d1714792e273b0d9fa
Artem <pachevartem@ya.ru>
2020-04-10 18:34:54 +0300

    first commit

---
+++ Assets/Scripts/InputBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InputBehaviour : MonoBehaviour
+{
+    [SerializeField] 
+    private InputField _inputField;
+
+    [SerializeField] 
+    private Dropdown _dropdown;
+
+    public List<string> words = new List<string>();
+    
+    void Start()
+    {
+        words = setupWords();
+        
+        _dropdown.options.Clear();
+//        _dropdown.options = datas(words);
+        _inputField.onValueChanged.AddListener(arg0 => tempp());
+        _dropdown.Show();
+//        _inputField.ActivateInputField();
+        
+    }
+
+    List<string> FindWords(string name)
+    {
+        var _resultList = new List<string>();
+
+        foreach (string s in words)
+        {
+            if (s.Contains(name))
+            {
+              _resultList.Add(s);  
+            }
+        }
+
+        return _resultList;
+    }
+
+    void Update()
+    {
+        _dropdown.options = datas(FindWords(_inputField.text));
+       
+    }
+
+
+    void tempp()
+    {
+
+        (_dropdown as MyDropDown).rrrrr(FindWords(_inputField.text));
+//        _dropdown.Hide();
+//        _dropdown.Show();
+        
+    }
+
+    List<string> setupWords()
+    {
+        var _tempList = new List<string>();
+        
+        _tempList.Add("artem");
+        _tempList.Add("anton");
+        _tempList.Add("vladimir");
+        _tempList.Add("vladlena");
+
+        return _tempList;
+    }
+    List<Dropdown.OptionData> datas(List<string> lib)
+    {
+        var _tempoptionData = new List<Dropdown.OptionData>();
+        foreach (string s in lib)
+        {
+            _tempoptionData.Add(new Dropdown.OptionData(s));
+        }
+        
+        return _tempoptionData;
+    }
+
+}
+
---
+++ Assets/Scripts/MyDropDown.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MyDropDown : Dropdown
+{
+    protected override void OnDidApplyAnimationProperties()
+    {
+        base.OnDidApplyAnimationProperties();
+    }
+
+    protected override void OnCanvasGroupChanged()
+    {
+        base.OnCanvasGroupChanged();
+    }
+
+    protected override void OnCanvasHierarchyChanged()
+    {
+        base.OnCanvasHierarchyChanged();
+    }
+
+    public override bool IsInteractable()
+    {
+        return base.IsInteractable();
+    }
+
+    protected override void OnRectTransformDimensionsChange()
+    {
+        base.OnRectTransformDimensionsChange();
+    }
+
+    protected override void InstantClearState()
+    {
+        base.InstantClearState();
+    }
+
+    protected override void DoStateTransition(SelectionState state, bool instant)
+    {
+        base.DoStateTransition(state, instant);
+    }
+
+    public override Selectable FindSelectableOnLeft()
+    {
+        return base.FindSelectableOnLeft();
+    }
+
+    public override Selectable FindSelectableOnRight()
+    {
+        return base.FindSelectableOnRight();
+    }
+
+    public override Selectable FindSelectableOnUp()
+    {
+        return base.FindSelectableOnUp();
+    }
+
+    public override Selectable FindSelectableOnDown()
+    {
+        return base.FindSelectableOnDown();
+    }
+
+    public override void OnMove(AxisEventData eventData)
+    {
+        base.OnMove(eventData);
+    }
+
+//    public override void OnPointerDown(PointerEventData eventData)
+//    {
+//        base.OnPointerDown(eventData);
+//    }
+//
+//    public override void OnPointerUp(PointerEventData eventData)
+//    {
+//        base.OnPointerUp(eventData);
+//    }
+//
+//    public override void OnPointerEnter(PointerEventData eventData)
+//    {
+//        base.OnPointerEnter(eventData);
+//    }
+//
+//    public override void OnPointerExit(PointerEventData eventData)
+//    {
+//        base.OnPointerExit(eventData);
+//    }
+//
+//    public override void OnSelect(BaseEventData eventData)
+//    {
+//        base.OnSelect(eventData);
+//    }
+//
+//    public override void OnDeselect(BaseEventData eventData)
+//    {
+//        base.OnDeselect(eventData);
+//    }
+
+    public override void Select()
+    {
+        base.Select();
+    }
+
+//    public override void OnPointerClick(PointerEventData eventData)
+//    {
+//        base.OnPointerClick(eventData);
+//    }
+//
+//    public override void OnSubmit(BaseEventData eventData)
+//    {
+//        base.OnSubmit(eventData);
+//    }
+//
+//    public override void OnCancel(BaseEventData eventData)
+//    {
+//        base.OnCancel(eventData);
+//    }
+
+    protected override GameObject CreateBlocker(Canvas rootCanvas)
+    {
+        Debug.Log("CreateBlocker");
+        var a = base.CreateBlocker(rootCanvas);
+        a.GetComponent<Image>().raycastTarget = false;
+        return a;
+//        return base.CreateBlocker(rootCanvas);
+    }
+
+    protected override void DestroyBlocker(GameObject blocker)
+    {
+        base.DestroyBlocker(blocker);
+    }
+
+    public void rrrrr(List<string> items)
+    {
+        foreach (var s in options)
+        {
+            var _tempDropdownItem = new DropdownItem();
+
+            if (_tempDropdownItem.text != null)
+            {
+                _tempDropdownItem.text.text = s.text;
+            }
+
+            DestroyItem(_tempDropdownItem);
+        }
+//        DestroyDropdownList(template.gameObject);
+
+        CreateDropdownList(template.gameObject);
+        foreach (string s in items)
+        {
+//            CreateItem(itemText.);
+        }
+    }
+
+    protected override GameObject CreateDropdownList(GameObject template)
+    {
+        Debug.Log("CreateDropdownList");
+        return base.CreateDropdownList(template);
+    }
+
+    protected override void DestroyDropdownList(GameObject dropdownList)
+    {
+        Debug.Log("DestroyDropdownList");
+
+        base.DestroyDropdownList(dropdownList);
+    }
+
+    protected override DropdownItem CreateItem(DropdownItem itemTemplate)
+    {
+        Debug.Log("CreateItem");
+
+        return base.CreateItem(itemTemplate);
+    }
+
+    protected override void DestroyItem(DropdownItem item)
+    {
+        Debug.Log("DestroyItem");
+
+        base.DestroyItem(item);
+    }
+}
